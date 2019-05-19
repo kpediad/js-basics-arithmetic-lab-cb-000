0203554a3d295f68bfc0ae2e255a67dc070ccb83
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-19 13:16:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-let newID = 1000000000;
-oldID += newID;
+let newID = oldID + 1000000000;
+
 let multiply = var1 * var2;
